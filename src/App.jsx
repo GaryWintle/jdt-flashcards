c147c646f7f7e4e5b0890d69d4eccec9fe6b1c9a
Gary Wintle <gwintle@gmail.com>
2025-05-25 19:42:22 +0900

    Added test cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,69 @@
+import { useState } from 'react';
+import './styles.css';
+
 function App() {
-  return <h1>Hi there!</h1>;
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: '緊急',
+    answer: 'きんきゅう - Emergency',
+  },
+  {
+    id: 7336,
+    question: '右左折',
+    answer: 'うさせつ - Turn right or left',
+  },
+  {
+    id: 8832,
+    question: '免許証',
+    answer: 'めんきょしょう - License ex. Drivers License',
+  },
+  {
+    id: 1297,
+    question: '有害物質',
+    answer: 'ゆうがいぶっしつ - Hazardous Substance/Material',
+  },
+  {
+    id: 9103,
+    question: '注意',
+    answer: 'ちゅうい - Attention/Caution',
+  },
+  {
+    id: 2002,
+    question: '迷惑',
+    answer: 'めいわく - Bothersome/Annoyance',
+  },
+];
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? 'selected' : ''}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
